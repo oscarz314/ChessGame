0b53353a8e8936961e842ede627551f1008a37d8
BT_C09_26 <mobileappdev@bths.edu>
2026-04-24 14:43:04 -0400

    Initial Commit

---
+++ ChessGame/ChessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessGameApp.swift
+//  ChessGame
+//
+//  Created by Student on 4/24/26.
+//
+
+import SwiftUI
+
+@main
+struct ChessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
